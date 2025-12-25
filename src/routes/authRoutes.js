a8d9439641c42620d8473a2b393ed6c062ac845a
Traman <tramgup@gmail.com>
2025-12-25 17:39:09 -0500

    fixed the routes to account for postgres and jwt

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,56 +5,63 @@ import db from '../db.js'
 
 const router = express.Router()
 
+//function that creates a JWT sign in access Token
+function signAccessToken(user){
+    return jwt.sign(
+        { sub: user.id, email: user.email},
+        process.env.JWT_SECRET,
+        { expiresIn: "24h"}
+    );
+}
+
 // Register a new user endpoing /auth/register
-router.post('/register', (req, res) => {
-    const { username, password } = req.body
-    // save the username and an irreversibly encrypted password
-    // save gilgamesh@gmail.com | aklsdjfasdf.asdf..qwe..q.we...qwe.qw.easd
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body
+    // save the username and encrypted password
 
     // encrypt the password
     const hashedPassword = bcrypt.hashSync(password, 8)
 
     // save the new user and hashed password to the db
     try {
-        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
-        const result = insertUser.run(username, hashedPassword)
-
-        // now that we have a user, I want to add their first todo for them
-        const defaultTodo = `Hello :) Add your first todo!`
-        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
-        insertTodo.run(result.lastInsertRowid, defaultTodo)
+        const user = await prisma.user.create({
+            data: {
+                email,
+                hashedPassword,
+                cart: { create: {} },
+            }
+    });
 
         // create a token
-        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
-        res.json({ token })
+        const token = signAccessToken(user);
+        return res.status(201).json({ token });
+
     } catch (err) {
         console.log(err.message)
         res.sendStatus(503)
     }
-})
+});
 
-router.post('/login', (req, res) => {
-    // we get their email, and we look up the password associated with that email in the database
-    // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
-    // so what we can to do, is again, one way encrypt the password the user just entered
+router.post('/login', async (req, res) => {
 
-    const { username, password } = req.body
+    const { email, password } = req.body
 
     try {
-        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
-        const user = getUser.get(username)
+        const user = await prisma.user.findUnique({
+            where: {email}
+        });
+        
+        //user not found
+        if (!user) return res.status(404).json({message: "User not found"});
 
-        // if we cannot find a user associated with that username, return out from the function
-        if (!user) { return res.status(404).send({ message: "User not found" }) }
+        //compare passwords and if invalid return that
+        const ok = await bcrypt.compare(password, user.passwordHash);
+        if (!ok) return res.status(401).json({message: "Invalid password"});
 
-        const passwordIsValid = bcrypt.compareSync(password, user.password)
-        // if the password does not match, return out of the function
-        if (!passwordIsValid) { return res.status(401).send({ message: "Invalid password" }) }
-        console.log(user)
+        const token = signAccessToken(user);
 
-        // then we have a successful authentication
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
-        res.json({ token })
+        return res.status(200).json({token, user : {id: user.id, email: user.email}});
+
     } catch (err) {
         console.log(err.message)
         res.sendStatus(503)
