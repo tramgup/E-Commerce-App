b6788f3d8e2dc25da021de4ec38dab91e309781d
Traman <tramgup@gmail.com>
2025-12-26 20:02:17 -0500

    added all this stuff back

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import shoppingRoutes from './routes/shoppingRoutes.js'
+import adminRoutes from './routes/adminRoutes.js'
+import authMiddleware from './middleware/authMiddleware.js'
+import adminMiddleware from './middleware/adminMiddleware.js'
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Routes, prepends /auth to all authroutes, and /todos to all todoRoutes
+app.use('/auth', authRoutes);
+app.use('/shopping', authMiddleware, shoppingRoutes);
+
+//admin routes for product updating and such
+app.use('/admin', authMiddleware, adminMiddleware, adminRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`)
+});
