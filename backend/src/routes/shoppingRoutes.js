54b79b6840e0f31c6c6553678386dd7e44f30edc
Traman <tramgup@gmail.com>
2026-01-07 18:59:33 -0500

    removed bad imports

--- backend/src/routes/shoppingRoutes.js
+++ backend/src/routes/shoppingRoutes.js
@@ -1,11 +1,8 @@
 import express from 'express';
 import prisma from '../prismaClient.js';
-import Stripe from "stripe";
 import dotenv from "dotenv";
 
 dotenv.config()
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-
 
 const router = express.Router()
 
